97df79a606bd6529a52b706cc051de51a51237a9
homura <homura.dev@gmail.com>
2020-04-16 22:48:29 +0800

    exclude build test file

--- packages/app/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const lib_a_1 = require("@foo/lib-a");
-function main() {
-    console.log(lib_a_1.add(1, 1));
-}
-main();
-//# sourceMappingURL=index.js.map
